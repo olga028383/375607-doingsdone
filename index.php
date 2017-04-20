533c7c25e80b8b119dd9b4f6be8ec4ef9d2b913f
Olga <olga028383@mail.ru>
2017-04-21 00:19:33 +0300

    Cleared the spaces

--- index.php
+++ index.php
@@ -146,7 +146,7 @@ if($days_until_deadline <= 0){
               <?php endif ?>
 
               <!--добавьте здесь класс "task--important" если эта задача просрочена-->
-              <tr class="tasks__item task  <?php echo $taskWarning; ?>">
+              <tr class="tasks__item task <?php echo $taskWarning; ?> ">
                 <td class="task__select">
                   <label class="checkbox task__checkbox">
                     <input class="checkbox__input visually-hidden" type="checkbox">
