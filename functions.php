db8d3a98f29f2681399e1998439795df5cb6916d
Olga <olga028383@mail.ru>
2017-04-28 00:13:10 +0300

    buffer

--- functions.php
+++ functions.php
@@ -4,6 +4,11 @@ function includeTemplate($template, $array)
     if(!isset($template)){
         return "";
     }
+    ob_start();
     
     require_once __DIR__."/templates/$template";
+    
+    $html = ob_get_clean();
+    
+    return $html;
 }
