f9cf85f2851617ffac945ae618dce7c077c8baba
Olga <olga028383@mail.ru>
2017-05-05 16:38:57 +0300

    database creation

---
+++ schema.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE `thingsarefine`;
+USE `thingsarefine`;
+CREATE TABLE `project` (
+`id` INT AUTO_INCREMENT PRIMARY KEY,
+`name` CHAR(255)
+);
+CREATE TABLE `tasks` (
+`id` INT AUTO_INCREMENT PRIMARY KEY,
+`user_id` INT,
+`project_id` INT,
+`dt_add` DATETIME,
+`dt_delete` DATETIME,
+`name` CHAR(255),
+`file` CHAR(255),
+`dt_life` DATETIME
+);
+CREATE TABLE `user` (
+`id` INT AUTO_INCREMENT PRIMARY KEY,
+`task_id` INT,
+`project_id` INT,
+`dt_register` DATETIME,
+`email`CHAR(155),
+`name` CHAR(155),
+`file` CHAR(100),
+`password` CHAR(32),
+`contants` CHAR(255)
+);
+CREATE TABLE `role` (
+`id` INT AUTO_INCREMENT PRIMARY KEY,
+`anonymous` TINYINT(1)
+);
+CREATE UNIQUE INDEX nameproject ON project(name);
+CREATE UNIQUE INDEX email ON user(email);
+CREATE INDEX name ON tasks(name);
+CREATE INDEX dt_add ON tasks(dt_add);
+CREATE INDEX dt_delete ON tasks(dt_delete);
+CREATE INDEX dt_life ON tasks(dt_life);
