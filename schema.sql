bcd52c8b4f8ec233497defec18f461e8772feace
Olga <olga028383@mail.ru>
2017-05-05 17:48:59 +0300

    Change fields

--- schema.sql
+++ schema.sql
@@ -2,26 +2,25 @@ CREATE DATABASE `thingsarefine`;
 USE `thingsarefine`;
 CREATE TABLE `projects` (
 `id` INT AUTO_INCREMENT PRIMARY KEY,
-`name` CHAR(255)
+`name` CHAR(255),
+`user_id` INT
 );
 CREATE TABLE `tasks` (
 `id` INT AUTO_INCREMENT PRIMARY KEY,
 `user_id` INT,
 `project_id` INT,
-`dt_add` DATETIME,
-`dt_delete` DATETIME,
+`dt_created` DATETIME,
+`dt_complete` DATETIME,
+`dt_deadline` DATETIME,
 `name` CHAR(255),
-`file` CHAR(255),
-`dt_life` DATETIME
+`file` CHAR(255)
 );
 CREATE TABLE `user` (
 `id` INT AUTO_INCREMENT PRIMARY KEY,
-`task_id` INT,
-`project_id` INT,
-`dt_register` DATETIME,
+`dt_registered` DATETIME,
 `email`CHAR(155),
 `name` CHAR(155),
-`file` CHAR(100),
+`avatar` CHAR(100),
 `password` CHAR(32),
 `contants` CHAR(255)
 );
@@ -29,9 +28,9 @@ CREATE TABLE `role` (
 `id` INT AUTO_INCREMENT PRIMARY KEY,
 `anonymous` TINYINT(1)
 );
-CREATE UNIQUE INDEX nameproject ON project(name);
+CREATE UNIQUE INDEX nameproject ON projects(name);
 CREATE UNIQUE INDEX email ON user(email);
 CREATE INDEX name ON tasks(name);
-CREATE INDEX dt_add ON tasks(dt_add);
-CREATE INDEX dt_delete ON tasks(dt_delete);
-CREATE INDEX dt_life ON tasks(dt_life);
+CREATE INDEX dt_created ON tasks(dt_created);
+CREATE INDEX dt_complete ON tasks(dt_complete);
+CREATE INDEX dt_deadline ON tasks(dt_deadline);
