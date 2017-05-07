1bcd3304a83e4b010660a8cbcd9a300c75c3fbaf
Olga <olga028383@mail.ru>
2017-05-07 16:51:49 +0300

    Sql queries

--- queries.sql
+++ queries.sql
@@ -3,12 +3,12 @@ SELECT * FROM `projects` WHERE user_id = 1;
 /*Выбрать все задачи для определенного проекта*/
 SELECT * FROM `tasks` WHERE project_id = 1;
 /*Пометить задачу как выполненную*/
-UPDATE `tasks` SET dt_complete = now() WHERE id = 1;
+UPDATE `tasks` SET complete = now() WHERE id = 1;
 /*Запрос добавляет новый проект, но здесь неправильно здесь нужно доп вставить id  пользователя найленного по его email, наверно*/
 INSERT INTO projects(name,user_id) VALUES ('Название',1);
 /*Вставить новую задачу*/
-INSERT INTO tasks(user_id, project_id, dt_created, dt_deadline, name) VALUES (1, 1, CURDATE(), CURDATE()+ INTERVAL 1 DAY, 'Какая-то новая задача1');
+INSERT INTO tasks(user_id, project_id, created, deadline, name) VALUES (1, 1, CURDATE(), CURDATE()+ INTERVAL 1 DAY, 'Какая-то новая задача1');
 /*Получить все задачи для завтрашнего дня*/
-SELECT * FROM `tasks` WHERE dt_deadline = CURDATE() + INTERVAL 1 DAY;
+SELECT * FROM `tasks` WHERE deadline = CURDATE() + INTERVAL 1 DAY;
 /*обновить название задачи по её идентификатору*/
 UPDATE tasks SET name = 'Новое значение' WHERE id = 1; 
--- schema.sql
+++ schema.sql
@@ -9,15 +9,15 @@ CREATE TABLE `tasks` (
 `id` INT AUTO_INCREMENT PRIMARY KEY,
 `user_id` INT NOT NULL,
 `project_id` INT NOT NULL,
-`dt_created` DATE NOT NULL,
-`dt_complete` DATE,
-`dt_deadline` DATE,
+`created` DATE NOT NULL,
+`complete` DATE,
+`deadline` DATE,
 `name` CHAR(255) NOT NULL,
 `file` CHAR(255)
 );
 CREATE TABLE `user` (
 `id` INT AUTO_INCREMENT PRIMARY KEY,
-`dt_registered` DATE NOT NULL,
+`registered` DATE NOT NULL,
 `email`CHAR(155) NOT NULL,
 `name` CHAR(155) NOT NULL,
 `avatar` CHAR(100),
