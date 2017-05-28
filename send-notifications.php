1b676984036028188c0728b0b784a543f91cc6e7
Olga <olga028383@mail.ru>
2017-05-28 22:54:54 +0300

    Added a mailing list

--- send-notifications.php
+++ send-notifications.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 require_once 'init.php';
 $time = date('Y-m-d H:i:s', time() - 3600);
-$sql = "SELECT tasks.name, tasks.deadline, user.email, user.name as user
+$sql = "SELECT tasks.id, tasks.name, tasks.deadline, user.email, user.name as user
         FROM tasks
         LEFT OUTER JOIN notifications
         ON tasks.id = notifications.task_id 
@@ -12,7 +12,10 @@ $sql = "SELECT tasks.name, tasks.deadline, user.email, user.name as user
         AND tasks.complete IS NULL
         AND tasks.deadline >= ?";
 $result = Database::instance()->getData($sql, [$time]);
-/*составляем массив пользователей и сообщений*/
+/*составляем массив пользователей и сообщений  и делаем отметку в таблицу notifications*/
+if (!$result) {
+    return;
+}
 $arr = array();
 foreach ($result as $value) {
     if (array_key_exists($value['email'], $arr)) {
@@ -21,16 +24,34 @@ foreach ($result as $value) {
         $arr[$value['email']]['text'] = '<li>Задача ' . $value['name'] . ' на ' . $value['deadline'] . '</li>';
         $arr[$value['email']]['name'] = $value['user'];
     }
+    $sqlAddNotification = "INSERT INTO notifications(task_id, sent_on) VALUES ( ?, NOW())";
+    Database::instance()->setData($sqlAddNotification, [$value['id']]);
 }
 /*рассылаем письма*/
 foreach ($arr as $key => $value) {
-    mail($key, 'Уведомление от сервиса «Дела в порядке»', message($value['name'], $value['text']));
+    sendMail($key, message($value['name'], $value['text']));
 }
 /**
  * Функция составляет сообщение
+ * @param string $name имя пользователя
+ * @param string $task задачи
  * @return string
  */
-function message($name, $text)
+function message($name, $task)
 {
-    return "<h1>Уважаемый пользователь, $name</h1><p>У Вас запланировано </p><ul>$text</ul>";
+    return "<h1>Уважаемый пользователь, $name</h1><p>У Вас запланировано </p><ul>$task</ul>";
+}
+
+/**
+ * Функция записывает в файл рассылаемые сообщения
+ * @param string $email
+ * @param string $message
+ */
+function sendMail($email, $message)
+{
+    $file = 'mail.html';
+    $handler = fopen($file, "a");
+    $text = "Пользователь : $email . Сообщение: $message";
+    fwrite($handler, iconv("UTF-8", "UTF-8", "$text"));
+    fclose($handler);
 }
